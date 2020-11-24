35a87505f725aae47de8686894c8f366f4fc81f4
cherubina-pantano <cherubina.pantano@gmail.com>
2020-11-24 16:14:44 +0100

    Struttura, style e chiamata API

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('oook js');
+const app = new Vue({
+  el: '#app',
+  data: {
+    albums: [],
+  },
+  created() {
+    // Ottenere dati dalle API
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then( response => {
+        // successo
+        console.log(response.data);
+        this.albums = response.data.response;
+      })
+      .catch(error => {
+        // in caso di errore
+        console.log(error);
+      });
+  },
+});
